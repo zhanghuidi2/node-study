12370fa6f5160a7c6b25f5bd71f09c5603399b62
zhanghuidi <781152188@qq.com>
2021-05-31 10:06:13 +0800

    koa-router

--- koa.js
+++ koa.js
@@ -1,23 +1,41 @@
 const Koa = require('koa')
+const Router = require('koa-router'); // 引入koa-router
+
 const app = new Koa()
-app.use(async (cxt, next) => {
-  const start = Date.now()
-  await next()
-  const end = Date.now()
-  // cxt.body = {
-  //   txt: `请求耗时${end-start}`
-  // }
+const router = new Router() // 创建路由，支持传递参数
+router.get('/', async (ctx) => {
+  ctx.type = 'html';
+  ctx.body = '<h1>hello world!</h1>';
 })
-app.use((cxt, next) => {
-  const express = Date.now() + 100
-  let i = 0
-  while (express > Date.now())
-  i++
-  cxt.body = {
-    name: i
-  }
-})
-app.listen(8888, () => {
+  .get("/users", async (ctx) => {
+    ctx.body = '获取用户列表';
+  })
+  .get("/users/:id", async (ctx) => {
+    const { id } = ctx.params
+    ctx.body = `获取id为${id}的用户`;
+  })
+  .post("/users", async (ctx) => {
+    ctx.body = `创建用户`;
+  })
+app.use(router.routes())
+// app.use(async (cxt, next) => {
+//   const start = Date.now()
+//   await next()
+//   const end = Date.now()
+//   // cxt.body = {
+//   //   txt: `请求耗时${end-start}`
+//   // }
+// })
+// app.use((cxt, next) => {
+//   const express = Date.now() + 100
+//   let i = 0
+//   while (express > Date.now())
+//   i++
+//   cxt.body = {
+//     name: i
+//   }
+// })
+app.listen(3000, () => {
   console.log('8888888')
 })
 
